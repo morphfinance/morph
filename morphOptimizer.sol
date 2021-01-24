b099c5b8fc32fd98ef4d517615f578ca658be820
0x2 <75550506+Code0x2@users.noreply.github.com>
2021-01-23 16:27:15 -0800

    Add files via upload

---
+++ morphOptimizer.sol
@@ -0,0 +1,398 @@
+// SPDX-License-Identifier: CC-BY-NC-SA-2.5
+
+//@code0x2
+
+pragma solidity ^0.6.12;
+
+import './files/contractguard.sol';
+import './files/operator.sol';
+import './files/safemath.sol';
+import './files/address.sol';
+import './files/ierc20.sol';
+
+interface ITreasury {
+    function getCurrentEpoch() external view returns (uint256);
+}
+
+library SafeERC20 {
+    using SafeMath for uint256;
+    using Address for address;
+
+    function safeTransfer(IERC20 token, address to, uint256 value) internal {
+        _callOptionalReturn(token, abi.encodeWithSelector(token.transfer.selector, to, value));
+    }
+
+    function safeTransferFrom(IERC20 token, address from, address to, uint256 value) internal {
+        _callOptionalReturn(token, abi.encodeWithSelector(token.transferFrom.selector, from, to, value));
+    }
+
+    /**
+     * @dev Deprecated. This function has issues similar to the ones found in
+     * {IERC20-approve}, and its usage is discouraged.
+     *
+     * Whenever possible, use {safeIncreaseAllowance} and
+     * {safeDecreaseAllowance} instead.
+     */
+    function safeApprove(IERC20 token, address spender, uint256 value) internal {
+        // safeApprove should only be called when setting an initial allowance,
+        // or when resetting it to zero. To increase and decrease it, use
+        // 'safeIncreaseAllowance' and 'safeDecreaseAllowance'
+        // solhint-disable-next-line max-line-length
+        require((value == 0) || (token.allowance(address(this), spender) == 0),
+            "SafeERC20: approve from non-zero to non-zero allowance"
+        );
+        _callOptionalReturn(token, abi.encodeWithSelector(token.approve.selector, spender, value));
+    }
+
+    function safeIncreaseAllowance(IERC20 token, address spender, uint256 value) internal {
+        uint256 newAllowance = token.allowance(address(this), spender).add(value);
+        _callOptionalReturn(token, abi.encodeWithSelector(token.approve.selector, spender, newAllowance));
+    }
+
+    function safeDecreaseAllowance(IERC20 token, address spender, uint256 value) internal {
+        uint256 newAllowance = token.allowance(address(this), spender).sub(value, "SafeERC20: decreased allowance below zero");
+        _callOptionalReturn(token, abi.encodeWithSelector(token.approve.selector, spender, newAllowance));
+    }
+
+    /**
+     * @dev Imitates a Solidity high-level call (i.e. a regular function call to a contract), relaxing the requirement
+     * on the return value: the return value is optional (but if data is returned, it must not be false).
+     * @param token The token targeted by the call.
+     * @param data The call data (encoded using abi.encode or one of its variants).
+     */
+    function _callOptionalReturn(IERC20 token, bytes memory data) private {
+        // We need to perform a low level call here, to bypass Solidity's return data size checking mechanism, since
+        // we're implementing it ourselves. We use {Address.functionCall} to perform this call, which verifies that
+        // the target address contains contract code and also asserts for success in the low-level call.
+
+        bytes memory returndata = address(token).functionCall(data, "SafeERC20: low-level call failed");
+        if (returndata.length > 0) { // Return data is optional
+            // solhint-disable-next-line max-line-length
+            require(abi.decode(returndata, (bool)), "SafeERC20: ERC20 operation did not succeed");
+        }
+    }
+}
+
+library Safe112 {
+    function add(uint112 a, uint112 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a, 'Safe112: addition overflow');
+
+        return c;
+    }
+
+    function sub(uint112 a, uint112 b) internal pure returns (uint256) {
+        return sub(a, b, 'Safe112: subtraction overflow');
+    }
+
+    function sub(
+        uint112 a,
+        uint112 b,
+        string memory errorMessage
+    ) internal pure returns (uint112) {
+        require(b <= a, errorMessage);
+        uint112 c = a - b;
+
+        return c;
+    }
+
+    function mul(uint112 a, uint112 b) internal pure returns (uint256) {
+        if (a == 0) {
+            return 0;
+        }
+
+        uint256 c = a * b;
+        require(c / a == b, 'Safe112: multiplication overflow');
+
+        return c;
+    }
+
+    function div(uint112 a, uint112 b) internal pure returns (uint256) {
+        return div(a, b, 'Safe112: division by zero');
+    }
+
+    function div(
+        uint112 a,
+        uint112 b,
+        string memory errorMessage
+    ) internal pure returns (uint112) {
+        // Solidity only automatically asserts when dividing by 0
+        require(b > 0, errorMessage);
+        uint112 c = a / b;
+
+        return c;
+    }
+
+    function mod(uint112 a, uint112 b) internal pure returns (uint256) {
+        return mod(a, b, 'Safe112: modulo by zero');
+    }
+
+    function mod(
+        uint112 a,
+        uint112 b,
+        string memory errorMessage
+    ) internal pure returns (uint112) {
+        require(b != 0, errorMessage);
+        return a % b;
+    }
+}
+
+contract ShareWrapper {
+    using SafeMath for uint256;
+    using SafeERC20 for IERC20;
+
+    IERC20 public share;
+
+    uint256 private _totalSupply;
+    mapping(address => uint256) private _balances;
+
+    function totalSupply() public view returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) public view returns (uint256) {
+        return _balances[account];
+    }
+
+    function stake(uint256 amount) public virtual {
+        _totalSupply = _totalSupply.add(amount);
+        _balances[msg.sender] = _balances[msg.sender].add(amount);
+        share.safeTransferFrom(msg.sender, address(this), amount);
+    }
+
+    function withdraw(uint256 amount) public virtual {
+        uint256 directorShare = _balances[msg.sender];
+        require(
+            directorShare >= amount,
+            'Boardroom: withdraw request greater than staked amount'
+        );
+        _totalSupply = _totalSupply.sub(amount);
+        _balances[msg.sender] = directorShare.sub(amount);
+        share.safeTransfer(msg.sender, amount);
+    }
+}
+
+contract morphOptimizer is ShareWrapper, ContractGuard, Operator {
+    using SafeERC20 for IERC20;
+    using Address for address;
+    using SafeMath for uint256;
+    using Safe112 for uint112;
+
+    /* ========== DATA STRUCTURES ========== */
+
+    struct Boardseat {
+        uint256 lastSnapshotIndex;
+        uint256 rewardEarned;
+    }
+
+    struct BoardSnapshot {
+        uint256 time;
+        uint256 rewardReceived;
+        uint256 rewardPerShare;
+    }
+
+    /* ========== STATE VARIABLES ========== */
+
+    IERC20 public cash;
+
+    mapping(address => Boardseat) private directors;
+    BoardSnapshot[] private boardHistory;
+    address payable internal creator;
+    uint256 public lockupEpochs;
+    mapping(address => uint256) public withdrawalEpoch;
+    address public treasury;
+
+    /* ========== CONSTRUCTOR ========== */
+
+    constructor(IERC20 _cash, IERC20 _share, uint256 _lockupEpochs) public {
+        cash = _cash;
+        share = _share;
+        lockupEpochs = _lockupEpochs;
+
+        BoardSnapshot memory genesisSnapshot = BoardSnapshot({
+            time: block.number,
+            rewardReceived: 0,
+            rewardPerShare: 0
+        });
+        boardHistory.push(genesisSnapshot);
+        
+        creator = msg.sender;
+    }
+
+    /* ========== Modifiers =============== */
+    modifier directorExists {
+        require(
+            balanceOf(msg.sender) > 0,
+            'Boardroom: The director does not exist'
+        );
+        _;
+    }
+
+    modifier updateReward(address director) {
+        if (director != address(0)) {
+            Boardseat memory seat = directors[director];
+            seat.rewardEarned = earned(director);
+            seat.lastSnapshotIndex = latestSnapshotIndex();
+            directors[director] = seat;
+        }
+        _;
+    }
+    
+    modifier canWithdraw() {
+        require(withdrawalEpoch[msg.sender] <= ITreasury(treasury).getCurrentEpoch(), "Boardroom: Cannot withdraw yet");
+        _;
+    }
+
+    /* ========== VIEW FUNCTIONS ========== */
+
+    // =========== Snapshot getters
+
+    function latestSnapshotIndex() public view returns (uint256) {
+        return boardHistory.length.sub(1);
+    }
+
+    function getLatestSnapshot() internal view returns (BoardSnapshot memory) {
+        return boardHistory[latestSnapshotIndex()];
+    }
+
+    function getLastSnapshotIndexOf(address director)
+        public
+        view
+        returns (uint256)
+    {
+        return directors[director].lastSnapshotIndex;
+    }
+
+    function getLastSnapshotOf(address director)
+        internal
+        view
+        returns (BoardSnapshot memory)
+    {
+        return boardHistory[getLastSnapshotIndexOf(director)];
+    }
+
+    // =========== Director getters
+
+    function rewardPerShare() public view returns (uint256) {
+        return getLatestSnapshot().rewardPerShare;
+    }
+
+    function earned(address director) public view returns (uint256) {
+        uint256 latestRPS = getLatestSnapshot().rewardPerShare;
+        uint256 storedRPS = getLastSnapshotOf(director).rewardPerShare;
+
+        return
+            balanceOf(director).mul(latestRPS.sub(storedRPS)).div(1e18).add(
+                directors[director].rewardEarned
+            );
+    }
+
+    /* ========== MUTATIVE FUNCTIONS ========== */
+    
+    function setNewTreasury(address _treasury) public onlyOperator {
+        treasury = _treasury;
+    }
+    
+    function setLockupEpochs(uint256 _epochs) public onlyOperator {
+        require(_epochs <= 8, "Boardroom: Epoch lock exceeds hardcap.");
+        lockupEpochs = _epochs;
+    }
+
+    function stake(uint256 amount)
+        public
+        override
+        onlyOneBlock
+        updateReward(msg.sender)
+    {
+        require(amount > 0, 'Boardroom: Cannot stake 0');
+        super.stake(amount);
+        withdrawalEpoch[msg.sender] = lockupEpochs.add(ITreasury(treasury).getCurrentEpoch());
+        emit Staked(msg.sender, amount);
+    }
+
+    function withdraw(uint256 amount)
+        public
+        override
+        onlyOneBlock
+        directorExists
+        updateReward(msg.sender)
+        canWithdraw
+    {
+        require(amount > 0, 'Boardroom: Cannot withdraw 0');
+        super.withdraw(amount);
+        emit Withdrawn(msg.sender, amount);
+    }
+
+    function exit() external {
+        withdraw(balanceOf(msg.sender));
+        claimReward();
+    }
+
+    function claimReward() public updateReward(msg.sender) canWithdraw {
+        uint256 reward = directors[msg.sender].rewardEarned;
+        if (reward > 0) {
+            directors[msg.sender].rewardEarned = 0;
+            cash.safeTransfer(msg.sender, reward);
+            emit RewardPaid(msg.sender, reward);
+        }
+    }
+    
+    function allocateFee(uint256 amount) external onlyOperator {
+        require(amount > 0, 'Boardroom: Cannot allocate 0');
+        require(
+            totalSupply() > 0,
+            'Boardroom: Cannot allocate when totalSupply is 0'
+        );
+
+        // Create & add new snapshot
+        uint256 prevRPS = getLatestSnapshot().rewardPerShare;
+        uint256 nextRPS = prevRPS.add(amount.mul(1e18).div(totalSupply()));
+
+        BoardSnapshot memory newSnapshot = BoardSnapshot({
+            time: block.number,
+            rewardReceived: amount,
+            rewardPerShare: nextRPS
+        });
+        boardHistory.push(newSnapshot);
+        emit RewardAdded(msg.sender, amount);
+    }
+
+    function allocateSeigniorage(uint256 amount)
+        external
+        onlyOneBlock
+        onlyOperator
+    {
+        require(amount > 0, 'Boardroom: Cannot allocate 0');
+        require(
+            totalSupply() > 0,
+            'Boardroom: Cannot allocate when totalSupply is 0'
+        );
+
+        // Create & add new snapshot
+        uint256 prevRPS = getLatestSnapshot().rewardPerShare;
+        uint256 nextRPS = prevRPS.add(amount.mul(1e18).div(totalSupply()));
+
+        BoardSnapshot memory newSnapshot = BoardSnapshot({
+            time: block.number,
+            rewardReceived: amount,
+            rewardPerShare: nextRPS
+        });
+        boardHistory.push(newSnapshot);
+
+        cash.safeTransferFrom(msg.sender, address(this), amount);
+        emit RewardAdded(msg.sender, amount);
+    }
+
+    /* ========== EVENTS ========== */
+
+    event Staked(address indexed user, uint256 amount);
+    event Withdrawn(address indexed user, uint256 amount);
+    event RewardPaid(address indexed user, uint256 reward);
+    event RewardAdded(address indexed user, uint256 reward);
+    
+    // Fallback rescue
+    
+    receive() external payable{
+        creator.transfer(msg.value);
+    }
+}
